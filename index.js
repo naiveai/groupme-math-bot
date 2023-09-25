b9a3080c44657681c6a1d588def4fa28e92d7736
Eshan <eshansingh@gmail.com>
2023-09-25 18:36:41 -0400

    First working version with direct message rendering

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const functions = require("@google-cloud/functions-framework");
+const { convert } = require("convert-svg-to-png");
 
 functions.http("mathRenderer", async (req, res) => {
     const MathJax = await require("mathjax").init({
@@ -7,6 +8,36 @@ functions.http("mathRenderer", async (req, res) => {
         }
     });
 
-    const svg = MathJax.asciimath2svg("x^2 + y^2 = z^2");
-    res.send(MathJax.startup.adaptor.outerHTML(svg));
+    const rendered_svg = MathJax.asciimath2svg(req.body.text);
+    const rendered_png = await convert(MathJax.startup.adaptor.innerHTML(rendered_svg), { scale: 10 });
+
+    const groupme_response = await fetch("https://image.groupme.com/pictures", {
+        method: "POST",
+        headers: {
+            "X-Access-Token": process.env.GROUPME_TOKEN,
+            "Content-Type": "image/png"
+        },
+        body: rendered_png
+    });
+    const groupme_response_json = await groupme_response.json();
+    const groupme_image_url = groupme_response_json.payload.picture_url;
+
+    console.log(groupme_image_url);
+
+    const resp = await fetch("https://api.groupme.com/v3/bots/post", {
+        method: "POST",
+        body: JSON.stringify({
+            bot_id: process.env.GROUPME_BOT_ID,
+            text: "",
+            attachments: [{
+                type: "image",
+                url: groupme_image_url
+            }]
+        })
+    });
+
+    console.log(resp);
+    console.log(await resp.json());
+
+    res.sendStatus(200);
 });
