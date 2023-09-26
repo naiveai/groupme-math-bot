d092fad1540933c7087ac2cea7d6b88e18f37158
Eshan <eshansingh@gmail.com>
2023-09-25 23:01:29 -0400

    Support multiple math expressions per message

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
 const functions = require("@google-cloud/functions-framework");
 const { convert } = require("convert-svg-to-png");
 
+const mathRegex = /`(?<expression>.*?)`/g;
+
 functions.http("mathRenderer", async (req, res) => {
+    const message = req.body.text;
+    const mathExpressions = Array.from(message.matchAll(mathRegex)).map(match => match.groups.expression);
+
+    if (mathExpressions.length === 0) {
+        res.sendStatus(204); // 204 is No Content
+        return;
+    }
+
     const MathJax = await require("mathjax").init({
         loader: {
             load: ['input/asciimath', 'output/svg']
         }
     });
 
-    const rendered_svg = MathJax.asciimath2svg(req.body.text);
-    const rendered_png = await convert(MathJax.startup.adaptor.innerHTML(rendered_svg), { scale: 10 });
+    let renderedPngs = [];
 
-    const groupme_response = await fetch("https://image.groupme.com/pictures", {
-        method: "POST",
-        headers: {
-            "X-Access-Token": process.env.GROUPME_TOKEN,
-            "Content-Type": "image/png"
-        },
-        body: rendered_png
-    });
-    const groupme_response_json = await groupme_response.json();
-    const groupme_image_url = groupme_response_json.payload.picture_url;
+    for (let mathExpression of mathExpressions) {
+        const rendered_svg = MathJax.asciimath2svg(mathExpression);
+        renderedPngs.push(await convert(MathJax.startup.adaptor.innerHTML(rendered_svg), { scale: 10 }));
+    }
 
-    await fetch("https://api.groupme.com/v3/bots/post", {
-        method: "POST",
-        body: JSON.stringify({
-            bot_id: process.env.GROUPME_BOT_ID,
-            text: "",
-            attachments: [{
-                type: "image",
-                url: groupme_image_url
-            }, {
-                type: "reply",
-                reply_id: req.body.id,
-                base_reply_id: req.body.id
-            }]
-        })
-    });
+    await Promise.all(renderedPngs.map(async (renderedPng) => {
+        const groupme_response = await fetch("https://image.groupme.com/pictures", {
+            method: "POST",
+            headers: {
+                "X-Access-Token": process.env.GROUPME_TOKEN,
+                "Content-Type": "image/png"
+            },
+            body: renderedPng
+        });
+        const groupme_response_json = await groupme_response.json();
+
+        await fetch("https://api.groupme.com/v3/bots/post", {
+            method: "POST",
+            body: JSON.stringify({
+                bot_id: process.env.GROUPME_BOT_ID,
+                text: "",
+                attachments: [{
+                    type: "image",
+                    url: groupme_response_json.payload.picture_url
+                }, {
+                    type: "reply",
+                    reply_id: req.body.id,
+                    base_reply_id: req.body.id
+                }]
+            })
+        });
+    }));
 
     res.sendStatus(200);
 });
